e39a5bc259dfbad78b80f1a1200730d54186bd70
Mohammad Zaryab <mohammadzaryab0@gmail.com>
2022-12-06 15:14:20 +0530

    Docker config

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+const basename = process.env.NODE_ENV === 'production' ? 'studio.dashtoon.com' : '/';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
